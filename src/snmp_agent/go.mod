525281908a618b1947b76665de3deb1339ed0925
naohaya <naohaya@cc.kyoto-su.ac.jp>
2024-12-10 16:41:05 +0900

    added initial set of code.

---
+++ src/snmp_agent/go.mod
@@ -0,0 +1,5 @@
+module snmp_agent
+
+go 1.22.3
+
+require github.com/twsnmp/gosnmp v1.32.2 // indirect
---
+++ src/snmp_agent/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	//	"github.com/gosnmp/gosnmp"
+	"github.com/twsnmp/gosnmp"
+)
+
+func main() {
+	// SNMP エージェント設定
+	g := &gosnmp.GoSNMP{}
+	g.Port = 161
+	g.Community = "public"
+	g.Version = gosnmp.Version2c
+	//	g.Logger := log.New(os.Stdout, "", log.LstdFlags)
+	a := &gosnmp.GoSNMPAgent{
+		Port:   161,
+		IPAddr: "0.0.0.0",
+		Logger: g.Logger,
+		Snmp:   g,
+	}
+
+	a.AddMibList(".1.3.6.1.2.1.1.1.0", gosnmp.OctetString, getSysDescr)
+
+	// サーバーを起動
+	go func() {
+		log.Println("Starting SNMP Agent...")
+		if err := a.Start(); err != nil {
+			log.Fatalf("Error starting SNMP Agent: %v", err)
+		}
+	}()
+	//	defer a.Snmp.Conn.Close()
+
+	// シグナルでの終了処理
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+
+	log.Println("Shutting down SNMP Agent...")
+}
+
+func getSysDescr(oid string) interface{} {
+	return "test"
+}
