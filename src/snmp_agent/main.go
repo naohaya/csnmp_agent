1f90ea3857725f16e21453f1106953a941607907
naohaya <naohaya@cc.kyoto-su.ac.jp>
2025-01-24 17:27:42 +0900

    added several MIB information using initMib().

--- src/snmp_agent/main.go
+++ src/snmp_agent/main.go
@@ -5,10 +5,13 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	//	"github.com/gosnmp/gosnmp"
 	"github.com/twsnmp/gosnmp"
 )
+
+var startTime time.Time
 
 func main() {
 	// SNMP エージェント設定
@@ -44,10 +47,42 @@ func main() {
 	log.Println("Shutting down SNMP Agent...")
 }
 
-func initMib(a *gosnmp.GoSNMPAgent) {
-	a.AddMibList(".1.3.6.1.2.1.1.1.0", gosnmp.OctetString, getSysDescr)
-}
-
+// function for responding requests
 func getSysDescr(oid string) interface{} {
 	return "test"
+}
+
+func getSysObjectID(oid string) interface{} {
+	return ".1.3.6.1.2.1.1.1.0"
+}
+
+func getSysUpTime(oid string) interface{} {
+	return uint32((time.Now().UnixNano() - startTime.UnixNano()) / (1000 * 1000 * 10))
+}
+
+func getSysContact(oid string) interface{} {
+	return "test sysContact"
+}
+
+func getSysName(oid string) interface{} {
+	return "test sysName"
+}
+
+func getSysLocation(oid string) interface{} {
+	return "test sysLocation"
+}
+
+func getSysServices(oid string) interface{} {
+	return 72
+}
+
+// function for initializing MIB
+func initMib(a *gosnmp.GoSNMPAgent) {
+	a.AddMibList(".1.3.6.1.2.1.1.1.0", gosnmp.OctetString, getSysDescr)
+	a.AddMibList(".1.3.6.1.2.1.1.2.0", gosnmp.ObjectIdentifier, getSysObjectID)
+	a.AddMibList(".1.3.6.1.2.1.1.3.0", gosnmp.TimeTicks, getSysUpTime)
+	a.AddMibList(".1.3.6.1.2.1.1.7.0", gosnmp.Integer, getSysServices)
+	a.AddMibList(".1.3.6.1.2.1.1.4.0", gosnmp.OctetString, getSysContact)
+	a.AddMibList(".1.3.6.1.2.1.1.5.0", gosnmp.OctetString, getSysName)
+	a.AddMibList(".1.3.6.1.2.1.1.6.0", gosnmp.OctetString, getSysLocation)
 }
