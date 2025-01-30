2da9cc2d71781ca36198317ddbda4c8083b268dd
Naohiro Hayashibara <naohaya@cc.kyoto-su.ac.jp>
2025-01-30 23:30:34 +0900

    simple sealing of sysDescr has been implemented and tested.

--- src/snmp_agent/go.mod
+++ src/snmp_agent/go.mod
@@ -2,4 +2,9 @@ module snmp_agent
 
 go 1.22.3
 
-require github.com/twsnmp/gosnmp v1.32.2 // indirect
+require (
+	github.com/twsnmp/gosnmp v1.32.2
+	golang.org/x/crypto v0.32.0
+)
+
+require github.com/stretchr/testify v1.10.0 // indirect
--- src/snmp_agent/main.go
+++ src/snmp_agent/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	//	"github.com/gosnmp/gosnmp"
-	"github.com/twsnmp/gosnmp"
+	"fmt" // for debug
+
+	"github.com/twsnmp/gosnmp" //	"github.com/gosnmp/gosnmp"
+	// "ecrypto" // Ego's crypto package
 )
 
 var startTime time.Time
+var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+var key = []byte("astaxie12798akljzmknm.ahkjkljl;k")
 
 func main() {
+
 	// SNMP エージェント設定
 	g := &gosnmp.GoSNMP{}
 	g.Port = 161
@@ -49,7 +56,28 @@ func main() {
 
 // function for responding requests
 func getSysDescr(oid string) interface{} {
-	return "test"
+	plaintext := []byte("test")
+	// create a new AES cipher block
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	// create a new CFB encrypter
+	cfb := cipher.NewCFBEncrypter(c, commonIV)
+	ciphertext := make([]byte, len(plaintext))
+	cfb.XORKeyStream(ciphertext, plaintext)
+	fmt.Printf("%s => %x\n", plaintext, ciphertext)
+
+	// create a new CFB decrypter
+	cfbdec := cipher.NewCFBDecrypter(c, commonIV)
+	plaintextCopy := make([]byte, len(plaintext))
+	cfbdec.XORKeyStream(plaintextCopy, ciphertext)
+	fmt.Printf("%x => %s\n", ciphertext, plaintextCopy)
+
+	//	return "test"
+	return string(ciphertext)
 }
 
 func getSysObjectID(oid string) interface{} {
